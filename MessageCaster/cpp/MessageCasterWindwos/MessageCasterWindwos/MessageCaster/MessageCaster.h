a62ffba75765895963fe105daacddddf9230172f
kimmo <hemakim0215@naver.com>
2025-03-04 22:56:09 +0900

    .

--- MessageCaster/cpp/MessageCasterWindwos/MessageCasterWindwos/MessageCaster/MessageCaster.h
+++ MessageCaster/cpp/MessageCasterWindwos/MessageCasterWindwos/MessageCaster/MessageCaster.h
@@ -7,22 +7,18 @@
 #include <stdexcept> // std::runtime_error
 
 
-
-// BaseMessage 구조체 정의
 #pragma pack(push, 1)
 struct BaseMessage {
-    uint32_t id; // 메시지 타입을 나타냄
+    uint32_t id;
 
     BaseMessage(uint32_t id) : id(id) {}
 
-    // 메시지 직렬화
     std::vector<uint8_t> serialize() const {
         std::vector<uint8_t> buffer(sizeof(BaseMessage));
         std::memcpy(buffer.data(), &id, sizeof(id));
         return buffer;
     }
 
-    // 바이너리 데이터를 역직렬화하여 BaseMessage 생성
     static BaseMessage deserialize(const std::vector<uint8_t>& buffer) {
         if (buffer.size() < sizeof(uint32_t)) {
             throw std::runtime_error("Buffer too short");
@@ -39,7 +35,7 @@ class MessageCaster
 public:
     MessageCaster();
     ~MessageCaster();
- 
+
 private:
     int MAX_BUUFER_SIZE = 2048;
 
