ed91f708fed6bbf8a33d7d71c347d52064a28fe4
kimmo <hemakim0215@naver.com>
2025-03-04 22:58:25 +0900

    .

--- MessageCaster/cpp/MessageCasterWindwos/MessageCasterWindwos/main.cpp
+++ MessageCaster/cpp/MessageCasterWindwos/MessageCasterWindwos/main.cpp
@@ -1,9 +1,15 @@
 #include "Messages/ExampleMessage.hpp"
 #include "MessageCaster/MessageCaster.h"
+#include "MessageCaster/TestNode.h"
 
 #include <iostream>
 
 int main()
 {
 	std::cout << "QuickShot Message Caster " << std::endl;
+
+	TestNode* testNode = new TestNode();
+	testNode->Init("127.0.0.1", 8080);
+	testNode->Run();
+
 }
